a5134397845d0a8c5578f1611bb2c7626554b8bc
j3dream <rivkaer@qq.com>
2020-10-27 09:47:01 +0800

    Squashed 'buildSrc/' changes from e9b8bfe..8022593
    8022593 chore: 修改 j3dream core 依赖包版本
    
    git-subtree-dir: buildSrc
    git-subtree-split: 8022593b181d10b9533f5f70dc59360e2ebe1d19

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 repositories {
     google()
     jcenter()
-    maven("http://47.105.177.125:8081/repository/maven-snapshots/")
+    maven("http://47.105.177.125:8081/repository/maven-releases/")
 }
 
 plugins {
@@ -10,5 +10,5 @@ plugins {
 
 dependencies{
     // 基础Java工具集合
-    implementation("com.j3dream:core:0.0.1-SNAPSHOT")
+    implementation("com.j3dream:core:0.0.1")
 }
