fb9d0d6fc64db07844eea536a0058a363bca2401
j3dream <rivkaer@qq.com>
2020-11-03 17:07:21 +0800

    chore: 扩展 BaseFragment 应用工具类

--- common/src/main/java/com/j3dream/android/common/base/BaseFragment.java
+++ common/src/main/java/com/j3dream/android/common/base/BaseFragment.java
@@ -1,17 +1,36 @@
 package com.j3dream.android.common.base;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.common.collect.Maps;
+import com.j3dream.android.common.R;
+import com.j3dream.android.common.annotate.BindEventBus;
 import com.j3dream.android.common.exception.InitContentViewException;
+import com.j3dream.android.common.interf.IViewLoading;
+import com.j3dream.android.common.interf.OnRequestPermissionsResultListener;
+import com.j3dream.android.common.util.DisplayUtils;
+import com.j3dream.android.common.util.IntentUtils;
+import com.j3dream.android.common.util.ToastUtils;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * <p>文件名称: BaseFragment </p>
@@ -22,12 +41,28 @@ import com.j3dream.android.common.exception.InitContentViewException;
  * @author <a href="mail to: cnrivkaer@outlook.com" rel="nofollow">lumo</a>
  * @version v1.0
  */
-public abstract class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment implements IViewLoading {
+
+    private static final int DEFAULT_REQUEST_PERMISSION_CODE = 11722;
 
     @Nullable
     protected View mContentView;
     @Nullable
     protected Context mContext;
+
+    /**
+     * 是否需要注册EventBus
+     */
+    private boolean isReceiveEventBus = Boolean.FALSE;
+    /**
+     * 基础的加载动画
+     */
+    private AlertDialog mLoadingDialog;
+
+    /**
+     * 信息提示的Dialog
+     */
+    private AlertDialog.Builder mBaseAlertDialog;
 
     /**
      * 设置当前布局资源
@@ -92,6 +127,183 @@ public abstract class BaseFragment extends Fragment {
             return (View) layoutRes;
         } else {
             throw new InitContentViewException("don't support u commit layout resource type!!!");
+        }
+    }
+
+    private Map<Integer, OnRequestPermissionsResultListener> mRequestPermissionsResultListeners
+            = Maps.newHashMap();
+
+    @Override
+    public void showLoading() {
+        if (mLoadingDialog == null) {
+            mLoadingDialog = new AlertDialog.Builder(mContext)
+                    .setCancelable(false)
+                    .create();
+        }
+        mLoadingDialog.show();
+        int dialogSideLength = DisplayUtils.getLongSideSize() / 8;
+        Window window = mLoadingDialog.getWindow();
+        if (window != null)
+            window.setLayout(dialogSideLength, dialogSideLength);
+        mLoadingDialog.setContentView(R.layout.widget_base_loading_activity);
+    }
+
+    @Override
+    public void hideLoading() {
+        if (mLoadingDialog != null) {
+            mLoadingDialog.hide();
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        if (mContentView != null) {
+            ViewGroup parent = (ViewGroup) mContentView.getParent();
+            if (parent != null) {
+                parent.removeView(mContentView);
+            }
+            mContentView = null;
+        }
+        if (mLoadingDialog != null) {
+            mLoadingDialog.dismiss();
+            mLoadingDialog = null;
+        }
+        if (mBaseAlertDialog != null) {
+            mBaseAlertDialog = null;
+        }
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        unBindEventBus();
+        super.onDestroy();
+    }
+
+    /**
+     * 获取基础的弹出层
+     *
+     * @return 基础的弹出层
+     */
+    protected androidx.appcompat.app.AlertDialog.Builder getBaseAlertDialog() {
+        if (mBaseAlertDialog == null) {
+            mBaseAlertDialog = new androidx.appcompat.app.AlertDialog.Builder(mContext);
+        }
+        return mBaseAlertDialog;
+    }
+
+    /**
+     * 检查是否拥有了相关权限
+     *
+     * @param permissions 检测的权限列表
+     * @return 是否已经拥有了相关权限
+     */
+    protected boolean checkSelfPermission(String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(mContext, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected void requestPermissions(OnRequestPermissionsResultListener requestPermissionsResultListener,
+                                      String... permissions) {
+        requestPermissions(requestPermissionsResultListener, DEFAULT_REQUEST_PERMISSION_CODE, permissions);
+    }
+
+    protected void requestPermissions(OnRequestPermissionsResultListener requestPermissionsResultListener,
+                                      int requestCode, String... permissions) {
+        mRequestPermissionsResultListeners.put(requestCode, requestPermissionsResultListener);
+        if (checkSelfPermission(permissions)) {
+            requestPermissionsResultListener.onResult(requestCode, permissions, Boolean.TRUE);
+        } else {
+            requestPermissions(permissions, requestCode);
+        }
+    }
+
+    /**
+     * 跳转到应用的详情信息设置页面
+     */
+    protected void launchAppDetailsSettings() {
+        Intent launchAppDetailsSettingsIntent = IntentUtils.getLaunchAppDetailsSettingsIntent();
+        startActivity(launchAppDetailsSettingsIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean grant = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        if (mRequestPermissionsResultListeners.containsKey(requestCode)) {
+            mRequestPermissionsResultListeners.get(requestCode)
+                    .onResult(requestCode, permissions, grant);
+        } else {
+            if (grant)
+                ToastUtils.show(R.string.grant_permission_message);
+            else
+                ToastUtils.show(R.string.not_grant_permission_message);
+        }
+    }
+
+    /**
+     * 获取内容View
+     *
+     * @param layoutRes   内容view
+     * @param container   容器
+     * @param attach2Root 是否绑定主视图
+     * @return 内容View
+     */
+    private View getFragmentContentView(
+            Object layoutRes,
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            boolean attach2Root
+    ) {
+        if (layoutRes instanceof View) {
+            return (View) layoutRes;
+        } else if (layoutRes instanceof Integer) {
+            return inflater.inflate((Integer) layoutRes, container, attach2Root);
+        } else {
+            throw new InitContentViewException("don't support u commit layout resource type!!!");
+        }
+    }
+
+    /**
+     * 判断是否符合EventBus注册要求
+     *
+     * @return {true: 符合注册要求, false: 不符合注册要求}
+     */
+    private boolean isAllBindEventBusRequire() {
+        Class<? extends BaseFragment> clazz = this.getClass();
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        if (!clazz.isAnnotationPresent(BindEventBus.class)) {
+            return Boolean.FALSE;
+        }
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.isAnnotationPresent(Subscribe.class)) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    private void bindEventBus() {
+        // 获取事件总线绑定许可
+        isReceiveEventBus = isAllBindEventBusRequire();
+        boolean isRegistered = EventBus.getDefault().isRegistered(this);
+        if (isReceiveEventBus && !isRegistered) {
+            // 事件总线许可通过, 进行绑定
+            EventBus.getDefault().register(this);
+        }
+    }
+
+    private void unBindEventBus() {
+        // 事件总线许可通过, 事件总线解绑
+        boolean isRegistered = EventBus.getDefault().isRegistered(this);
+        if (isRegistered) {
+            EventBus.getDefault().removeAllStickyEvents();
+            EventBus.getDefault().unregister(this);
         }
     }
 }
