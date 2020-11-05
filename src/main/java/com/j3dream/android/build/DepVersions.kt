e6a227fbd573440ae266d63cf7dbb6a8a0f256bb
j3dream <rivkaer@qq.com>
2020-11-05 14:54:10 +0800

    Squashed 'buildSrc/' changes from 58b1dcc..4772b42
    4772b42 chore: 完善相关部分的依赖列表
    6c19a54 chore: 修改部分依赖配置, 增加依赖使用
    d5f3304 chore: 增加常用的依赖环境结构
    
    git-subtree-dir: buildSrc
    git-subtree-split: 4772b42a09f3074f370903b2207d16aa00454216

--- src/main/java/com/j3dream/android/build/DepVersions.kt
+++ src/main/java/com/j3dream/android/build/DepVersions.kt
@@ -29,6 +29,8 @@ internal object DepVersions {
     const val retrofit = "2.3.0"
     const val retrofit_convert_rxjava = retrofit
     const val retrofit_convert_gson = retrofit
+    const val stetho = "1.5.1"
+    const val zxing = "3.3.0"
 
     object Commons {
         const val codec = "1.14"
@@ -37,7 +39,7 @@ internal object DepVersions {
 
     object Framework {
         const val core = "0.1.1"
-        const val common_android = "0.1.1@aar"
+        const val common_android = "0.1.2@aar"
         const val idreader_android = "0.1.1@aar"
         const val arcface_android = "0.1.1@aar"
         const val net_android = "0.1.1@aar"
@@ -52,14 +54,30 @@ internal object DepVersions {
 
     object AndroidX{
         const val core_ktx = "1.3.2"
+        const val concurrent_futures = "1.1.0"
+        const val arch_core = "2.1.0"
         const val appcompat = "1.2.0"
         const val constraintlayout = "2.0.2"
         const val multidex = "2.0.1"
         const val recyclerview = "1.1.0"
+        const val viewpager2 = "1.0.0"
+        const val emoji = "1.1.0"
+        const val startup = "1.0.0"
+        const val jetpack_room = "2.2.3"
+        const val jetpack_camera = "1.0.0-beta10"
+        const val jetpack_workmanager = "2.3.0"
+        const val jetpack_fragment = "1.2.5"
+        const val jetpack_lifecycle = "2.2.0"
+        const val jetpack_paging = "2.2.0"
+        const val material = "1.2.1"
+        const val exifinterface = "1.3.1"
+        const val cardview = "1.0.0"
     }
 
     object Tencent {
         const val qmui = "2.0.0-alpha10"
         const val bugly_crash = "2.8.6.0"
+        const val mmkv = "1.2.4"
+        const val x5tbs = "43939"
     }
 }
