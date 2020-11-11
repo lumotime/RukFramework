6811aeb86686e38b14aa193cc1b16673d79ad0e9
j3dream <rivkaer@qq.com>
2020-11-11 16:17:35 +0800

    Squashed 'buildSrc/' changes from 7c9377d..3d8f057
    3d8f057 feat: 更改BuildConstant默认版本号, Modules 增加版本号规范说明
    
    git-subtree-dir: buildSrc
    git-subtree-split: 3d8f057c349f3a214ab46f70172ac2df9d660ad5

--- src/main/java/com/j3dream/android/build/Modules.kt
+++ src/main/java/com/j3dream/android/build/Modules.kt
@@ -7,7 +7,8 @@ import com.j3dream.android.build.model.ModuleInfo
  * <p>所属包名: com.j3dream.android.build</p>
  * <p>描述:  </p>
  * <p>feature:
- *
+ *     VERSION_CODE_FORMAT: d_dd_ddd
+ *     VERSION_NAME_FORMAT: d.dd.ddd-s
  * </p>
  * <p>创建时间: 2020/10/23 15:47 </p>
  * @author <a href="mail to: cnrivkaer@outlook.com" rel="nofollow">lumo</a>
@@ -18,5 +19,5 @@ object Modules{
     /**
      * example android application build config
      */
-    val M_example = ModuleInfo("com.j3dream.android.example", 1001, "0.01.001")
+    val M_example = ModuleInfo("com.j3dream.android.example", 1, "0.0.1")
 }
--- src/main/java/com/j3dream/android/build/constant/BuildConstant.kt
+++ src/main/java/com/j3dream/android/build/constant/BuildConstant.kt
@@ -1,7 +1,5 @@
 package com.j3dream.android.build.constant
 
-import java.io.File
-
 /**
  * <p>文件名称: Constant </p>
  * <p>所属包名: com.j3dream.build.android.constant</p>
@@ -14,8 +12,8 @@ import java.io.File
  * @version v1.0
  */
 object BuildConstant{
-    const val DEF_MIN_VERSION_CODE = 1001
-    const val DEF_MIN_VERSION_NAME = "0.01.001"
+    const val DEF_MIN_VERSION_CODE = 1
+    const val DEF_MIN_VERSION_NAME = "0.0.1"
     const val DEF_COMPILE_SDK_VERSION = 29
     const val DEF_BUILD_TOOLS_VERSION = "29.0.3"
     const val DEF_MIN_SDK_VERSION = 21
